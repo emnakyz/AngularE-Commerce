f2a3c690d8241244ba98933674bc9c7e29923011
Emin Akyüz <emn.ak100@gmail.com>
2022-12-25 20:35:10 +0300

    Repository  Pattern Uygulama

---
+++ API.Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using API.Core.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        /// <summary>
+        /// Tüm ürünleri listeler
+        /// </summary>
+        /// <returns></returns>
+        Task<IReadOnlyList<Product>> GetProductAsync();
+    }
+}
---
+++ API.Infrastructure/Implements/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using API.Core.DbModels;
+using API.Core.Interfaces;
+using API.Infrastructure.DataContext;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Infrastructure.Implements
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StoreContext _context;
+        public ProductRepository(StoreContext context)
+        {
+            _context = context;
+        }
+        /// <summary>
+        /// Tüm Ürünleri Listeler
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IReadOnlyList<Product>> GetProductAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
