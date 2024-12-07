52d0980f8fc2f65e27a5ee9ccdc3cdb0efa05ca3
Emin Akyüz <emn.ak100@gmail.com>
2024-12-08 01:50:12 +0300

    Görsel Düzenleme 3-BreadCrumb Ekleme Ve Tema Değiştirme

--- client/src/app/core/section-header/section-header.component.ts
+++ client/src/app/core/section-header/section-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-section-header',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectionHeaderComponent implements OnInit {
 
-  constructor() { }
+  breadcrumbs$ : Observable<any[]>;
+  constructor(private breadCrumbService:BreadcrumbService) { }
 
   ngOnInit(): void {
+    this.breadcrumbs$ = this.breadCrumbService.breadcrumbs$;
   }
 
 }
