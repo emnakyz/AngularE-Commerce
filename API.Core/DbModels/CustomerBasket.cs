e55a8c302fbf78a864238ab9e97b67880819b86f
Emin Akyüz <emn.ak100@gmail.com>
2024-12-14 02:10:09 +0300

    Sepet classları eklendi

---
+++ API.Core/DbModels/CustomerBasket.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace API.Core.DbModels
+{
+	public class CustomerBasket
+	{
+		public CustomerBasket()
+		{
+			
+		}
+		public CustomerBasket(string id)
+		{
+			Id = id;
+		}
+		public string Id { get; set; }
+		public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+	}
+}
