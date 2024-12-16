50a55590677f35e2066ab30874f816805d852491
Emin Akyüz <emn.ak100@gmail.com>
2024-12-16 16:34:23 +0300

    BasketController,BasketRepository,IBasketRepository eklendi

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,19 +1,16 @@
 ﻿using API.Core.Interfaces;
 using API.Infrastructure.Implements;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace API.Extensions
 {
-    public static class ApplicationServiceExtensions
+	public static class ApplicationServiceExtensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+			services.AddScoped<IBasketRepository, BasketRepository>();
+			services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
             return services;
         }
     }
